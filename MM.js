d584b7b1774db9143285f9fad25de2e691b7e45a
Tearsillusion <1585345738@qq.com>
2018-01-23 11:26:06 +0800

    1

---
+++ MM.js
@@ -0,0 +1,6 @@
+var imgLeft=document.getElementById("img_Left");
+var iconL=document.getElementsByClassName("icon-icon-copy");
+var iconR=document.getElementsByClassName("icon-icon");
+var imgleftTop=document.getElementsByClassName("img_left_top");
+iconL.onclick=function(){
+    imgleftTop.style.left=50+"rem";
